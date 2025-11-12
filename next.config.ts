5725cc3c328c3749aeb53063d75580258806082d
kiddo_dong <dh655933@gmail.com>
2025-11-12 21:27:19 +0900

    Angler FE

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/Angler-Front' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Angler-Front/' : '',
 };
 
 export default nextConfig;
